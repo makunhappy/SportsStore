bfe37224e18a034bf9f30ef0fa91718bb4fa70a4
KunMa <furtherroads@gmail.com>
2018-10-09 18:21:11 +0800

    add

---
+++ SportsStore/Models/FakeProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.Models
+{
+    public class FakeProductRepository : IProductRepository
+    {
+        public IQueryable<Product> products => new List<Product>
+        {
+            new Product{ Name="Footbal",Price=25},
+            new Product{ Name="Surf board",Price=179},
+            new Product{ Name="Running shoes",Price=95}
+        }.AsQueryable<Product>();
+    }
+}
